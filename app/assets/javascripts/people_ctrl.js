74d6948ada01c47491ea13366b058bd518ec4b8e
Brian <riceb53@gmail.com>
2016-12-27 21:25:19 -0500

    Add angular to table

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -4,9 +4,7 @@
   angular.module("app").controller("peopleCtrl", function($scope, $http){
 
     $scope.setup = function(){
-      console.log('hello');
       $http.get("/api/v1/people").then(function(response){
-        console.log(response.data);
         $scope.people = response.data;
       });
     }
@@ -30,6 +28,31 @@
 
     }
 
+    $scope.getOrderedAttribute = function(inputAttribute){
+      if (inputAttribute === $scope.inputAttributeOrder) {
+        $scope.reverse = !$scope.reverse;
+        if (inputAttribute === 'name') {
+          if ($scope.reverse) {
+            $scope.nameSorter = 'v';
+          } else {
+            $scope.nameSorter = '^';
+          }
+        } else {
+            if ($scope.reverse) {
+              $scope.bioSorter = 'v';
+            } else {
+              $scope.bioSorter = '^';
+            }
+            $scope.nameSorter = '';
+        }
+
+      } else {
+        $scope.reverse = false;
+      }
+      //  otherwise sort it alphabetically
+      $scope.inputAttributeOrder = inputAttribute;
+    }
+
     $scope.showBio = function(inputPerson){
       // return true if biovisible is true otherwise return false
       if (inputPerson.bioVisible === true) {
@@ -46,19 +69,25 @@
       }
       // add them to the db
       $http.post('/api/v1/people', objectThing).then(function(response){
-        console.log(response);
         var newItem = response.data
         $scope.people.push(response.data);
         $scope.name = "";
         $scope.bio = "";
+      }, function(responseError){
+        $scope.errors = responseError.data.errors
       })
 
     }
 
-    $scope.removeHuman = function(someIndex){
+    $scope.removeHuman = function(somePerson){
+      console.log(somePerson);
+      var someIndex = $scope.people.indexOf(somePerson);
+      console.log(someIndex);
       $scope.people.splice(someIndex,1);
     }
 
+    // end of controller
   });
 
+// /end of app
 }());
